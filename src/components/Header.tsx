5c8a88e4c9961227c4f747e05e893b95f333a4d1
Sergionx <sergio180203@gmail.com>
2023-09-02 14:07:56 -0400

    Header changes darkMode

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import spainFlag from "../assets/es.svg";
+import usaFlag from "../assets/us.svg";
+
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
+import Switch from "./Switch";
+
+export default function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      console.log("hola");
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  return (
+    // REVIEW - Considerar hacerlo sticky
+    <header
+      className="absolute top-0 right-[20px] z-50 px-6 py-4
+      bg-normal-white/60 dark:bg-normal-white/30 backdrop-blur
+      rounded-b-2xl border-2 border-[#d8d8d8] dark:border-white/20 border-t-0 
+      flex gap-4 justify-between 
+    "
+    >
+      <section className="flex gap-2 items-center">
+        <img src={spainFlag} alt="Spain flag" className="w-6 h-6" />
+
+        <Switch
+          onCheck={(checked) => {
+            setIsDarkMode(checked);
+          }}
+        />
+
+        <img src={usaFlag} alt="USA flag" className="w-6 h-6" />
+      </section>
+
+      <div id="separator" className="border border-tertiary-400 dark:border-tertiary-950 h-[25px]"></div>
+
+      <section className="flex gap-2 items-center text-tertiary-400 dark:text-tertiary-950">
+        <BsFillSunFill alt="Sun" size={24} />
+
+        <Switch  onCheck={(checked) => {
+            setIsDarkMode(checked);
+          }} />
+
+        <BsFillMoonFill alt="Moon" size={24} />
+      </section>
+    </header>
+  );
+}
---
+++ src/components/Switch.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface Props {
+  onCheck: (checked: boolean) => void;
+}
+
+export default function Switch({ onCheck }: Props) {
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        value=""
+        className="sr-only peer"
+        onChange={(event) => {
+          onCheck(event.target.checked);
+        }}
+      />
+      <div
+        className="w-[3.25rem] h-6 border-2  rounded-full dark:border-gray-600 peer
+              peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-tertiary-300 dark:peer-focus:ring-tertiary-800 
+              peer-checked:after:translate-x-full peer-checked:after:border-white 
+              peer-checked:bg-tertiary-600 dark:peer-checked:bg-tertiary-800
+              after:content-[''] after:absolute after:top-[3px] after:left-[6px]
+              peer-checked:after:left-[12px]
+              after:bg-white after:border-gray-300 after:border after:rounded-full 
+              after:h-[18px] after:w-[18px] after:transition-all"
+      ></div>
+    </label>
+  );
+}
