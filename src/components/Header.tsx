228eec19b8334224062a68a209cc1dfd7599ec3a
Sergionx <sergio180203@gmail.com>
2023-09-04 19:54:25 -0400

    Binding checked property

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,15 +28,16 @@ export default function Header() {
     // REVIEW - Considerar hacerlo sticky
     <header
       className="absolute top-0 right-[20px] z-50 px-6 py-4
-      bg-normal-white/60 dark:bg-normal-white/30 backdrop-blur
-      rounded-b-2xl border-2 border-[#d8d8d8] dark:border-white/20 border-t-0 
-      flex gap-4 justify-between 
+        bg-normal-white/60 dark:bg-normal-white/30 backdrop-blur
+        rounded-b-2xl border-2 border-[#d8d8d8] dark:border-white/20 border-t-0
+        flex gap-4 justify-between 
     "
     >
       <section className="flex gap-2 items-center">
         <img src={spainFlag} alt="Spain flag" className="w-6 h-6" />
 
         <Switch
+          value={isDarkMode}
           onCheck={(checked) => {
             setIsDarkMode(checked);
           }}
@@ -45,14 +46,20 @@ export default function Header() {
         <img src={usaFlag} alt="USA flag" className="w-6 h-6" />
       </section>
 
-      <div id="separator" className="border border-tertiary-400 dark:border-tertiary-950 h-[25px]"></div>
+      <div
+        id="separator"
+        className="border border-tertiary-400 dark:border-tertiary-950 h-[25px]"
+      ></div>
 
       <section className="flex gap-2 items-center text-tertiary-400 dark:text-tertiary-950">
         <BsFillSunFill alt="Sun" size={24} />
 
-        <Switch  onCheck={(checked) => {
+        <Switch
+          value={isDarkMode}
+          onCheck={(checked) => {
             setIsDarkMode(checked);
-          }} />
+          }}
+        />
 
         <BsFillMoonFill alt="Moon" size={24} />
       </section>
--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -2,28 +2,29 @@ import React from "react";
 
 interface Props {
   onCheck: (checked: boolean) => void;
+  value: boolean;
 }
 
-export default function Switch({ onCheck }: Props) {
+export default function Switch({ onCheck, value }: Props) {
   return (
     <label className="relative inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
-        value=""
+        checked={value}
         className="sr-only peer"
         onChange={(event) => {
           onCheck(event.target.checked);
         }}
       />
       <div
-        className="w-[3.25rem] h-6 border-2  rounded-full dark:border-gray-600 peer
-              peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-tertiary-300 dark:peer-focus:ring-tertiary-800 
-              peer-checked:after:translate-x-full peer-checked:after:border-white 
-              peer-checked:bg-tertiary-600 dark:peer-checked:bg-tertiary-800
-              after:content-[''] after:absolute after:top-[3px] after:left-[6px]
-              peer-checked:after:left-[12px]
-              after:bg-white after:border-gray-300 after:border after:rounded-full 
-              after:h-[18px] after:w-[18px] after:transition-all"
+        className="w-[3.25rem] h-6 border-2 border-gray-300 dark:border-gray-600 rounded-full peer
+          peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-tertiary-300 dark:peer-focus:ring-tertiary-800 
+          peer-checked:after:translate-x-full peer-checked:after:border-white 
+          peer-checked:bg-tertiary-600 dark:peer-checked:bg-tertiary-800
+          after:content-[''] after:absolute after:top-[3px] after:left-[6px]
+          peer-checked:after:left-[12px]
+          after:bg-white after:border-gray-300 after:border after:rounded-full 
+          after:h-[18px] after:w-[18px] after:transition-all"
       ></div>
     </label>
   );
