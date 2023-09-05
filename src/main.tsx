03303ef1035e5fb867dbc583de0a82dd3eada332
Sergionx <sergio180203@gmail.com>
2023-09-05 19:09:18 -0400

    added sideMenu

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Header from "./components/layout/Header.tsx";
+import SideMenu from "./components/layout/SideMenu.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SideMenu />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
