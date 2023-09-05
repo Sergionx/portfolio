367e58ed4788443df7864db2ebb36fce187924f7
Sergionx <sergio180203@gmail.com>
2023-09-05 19:09:25 -0400

    xd

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Header from "./components/layout/Header.tsx";
 import SideMenu from "./components/layout/SideMenu.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
