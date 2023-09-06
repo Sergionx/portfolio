177c24c69a4a92ff55d060aff9a38ec0134fdcbc
Sergionx <sergio180203@gmail.com>
2023-09-06 19:41:07 -0400

    About section

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import About from "./components/sections/About";
+import Experience from "./components/sections/Experience";
 import Hero from "./components/sections/Hero";
 import Projects from "./components/sections/Projects";
+import 'react-tooltip/dist/react-tooltip.css'
 
 function App() {
   return (
     <main
-      className="flex flex-col items-center  min-h-[200vh] px-10
+      className="flex flex-col items-center  min-h-[200vh] px-8 md:px-48
       dark:text-normal-white text-normal-black bg-normal-white dark:bg-normal-black
       "
       // transition-colors duration-500
     >
-      <section className="p-5 max-w-3xl flex items-center  mt-64">
+      <section className="p-5 max-w-3xl flex items-center  pt-64" id="Home">
         <Hero />
       </section>
 
-      <section className="">
+      <section className="flex flex-col items-center pt-64"  id="Projects">
         <Projects />
+      </section>
+
+      <section className="flex flex-col items-center pt-64"  id="Experience">
+        <Experience />
+      </section>
+
+      <section id="About" className="flex flex-col pt-64">
+        <About />
       </section>
     </main>
   );
