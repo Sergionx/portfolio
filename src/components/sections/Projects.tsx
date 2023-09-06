ddb09d70028393c965a1ba0e02619449cf592b6f
Sergionx <sergio180203@gmail.com>
2023-09-06 19:42:38 -0400

    Used tooltip and populated list

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,70 +1,157 @@
 import { BiLinkExternal } from "react-icons/bi";
-import { AiFillGithub } from "react-icons/ai";
+import { AiFillGithub, AiFillLock } from "react-icons/ai";
+import { BsFillBuildingFill } from "react-icons/bs";
+import { Tooltip } from "react-tooltip";
+
+interface Project {
+  name: string;
+  description: string;
+  technologies: string[];
+  url?: string;
+  githubUrl?: string;
+  date: string;
+}
 
 export default function Projects() {
-  const projects = [
+  const projects: Project[] = [
     {
-      name: "Proyecto 1",
+      name: "ChatGPT implementation",
       description:
-        "My Awesome Project is a web application that allows users to create and share their own custom playlists. With a sleek and intuitive user interface, users can easily search for and add songs to their playlists, reorder their playlists, and share their playlists with others. The application also includes a social feature that allows users to follow other users and discover new playlists. My Awesome Project is built using React, TypeScript, and Firebase, and is designed to be responsive and accessible on all devices. Whether you're a music lover looking to create the perfect playlist, or just looking for some new music to discover, My Awesome Project has you covered!",
+        "Integrated ChatGPT API on a web based chat system, using React, Tailwind and Firebase as a backend.",
+      technologies: ["React", "TypeScript", "TailwindCSS", "Firebase", "Vite"],
+      date: "2021-09-01",
+    },
+    {
+      name: "Ticket Management System",
+      description:
+        "Tasked with developing the FAQ module. Managing the creation, editing and overall presentation of articles.",
       technologies: [
-        "React",
+        "Angular",
         "TypeScript",
+        "SASS",
         "TailwindCSS",
         "Firebase",
-        "Vite",
-        "Mooook",
+        "PrimeNG",
+        "RxJS",
       ],
-      url: "https://www.google.com",
-      githubUrl: "https://www.yahoo.com",
-      date: "2021-09-01",
+      date: "todo",
+    },
+    {
+      name: "ERP System",
+      description:
+        "Tasked with refactoring several views and maintaining to separate modules within the whole ecosystem.",
+      technologies: ["Angular", "TypeScript", "SASS", "Firebase", "RxJS"],
+      date: "todo",
+    },
+    {
+      name: "Wova",
+      description:
+        "Designed and developed a landing page for a modern architecture company.",
+      technologies: ["React", "TypeScript", "TailwindCSS", "Firebase", "Vite"],
+      url: "https://wova-87e6e.web.app/",
+      githubUrl: "https://github.com/ValeriaZampetti/wova-studio/tree/main",
+      date: "todo",
     },
   ];
 
   const projectsCard = projects.map((project, index) => (
-    <article
-      className="bg-blue-100 dark:bg-blue-900 transition-all duration-300
+    <div key={index}>
+      <Tooltip
+        id={"private" + index}
+        place="bottom"
+        content="This is a private repository"
+        className="z-50"
+        clickable={true}
+      />
+
+      <article
+        className="bg-blue-100 dark:bg-blue-900 transition-all duration-300
       hover:bg-blue-100 dark:hover:bg-blue-900/90 hover:scale-105 group
-        max-w-lg w-full p-4 rounded-md shadow-md relative flex flex-col"
-      key={index}
-    >
-      <a
-        className="flex items-center gap-4 text-primary-600 dark:text-primary-500"
-        href={project.url}
-        target="_blank"
-        rel="noreferrer"
+        max-w-md w-full p-4 rounded-md shadow-md relative flex flex-col"
       >
-        <h6 className="font-semibold group-hover:font-bold">{project.name}</h6>{" "}
-        <BiLinkExternal size={18} className="inline-block" />
-      </a>
+        <BsFillBuildingFill
+          size={18}
+          data-tooltip-id={"private" + index}
+          className={`absolute top-4 right-4 text-normal-black/80 dark:text-normal-white/70
+        ${project.githubUrl ? "hidden" : "block"}`}
+        />
 
-      <span className="text-normal-black/80 dark:text-normal-white/70 mt-2">
-        {project.date}
-      </span>
+        <a
+          className="flex  items-center gap-4 text-primary-600 dark:text-primary-500 pr-8"
+          href={project.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h6 className="font-semibold group-hover:font-bold">
+            {project.name}
+          </h6>{" "}
+          <AiFillLock
+            size={18}
+            className={`shrink-0 ${
+              project.githubUrl ? "hidden" : "block"
+            } active:animate-shake`}
+          />
+          <BiLinkExternal
+            size={18}
+            className={`shrink-0 ${project.githubUrl ? "block" : "hidden"}`}
+          />
+        </a>
 
-      <p className="font-medium mt-4 max-w-prose break-words w-full">
-        {project.description}
-      </p>
+        <span className="text-normal-black/80 dark:text-normal-white/70 mt-2">
+          {project.date}
+        </span>
 
-      <ul className="flex flex-wrap items-center gap-2 font-bold text-sm mt-4 pr-10 ">
-        {project.technologies.map((technology) => (
-          <li className="bg-tertiary-300 text-tertiary-800 py-1 px-3 rounded-xl">
-            {technology}
-          </li>
-        ))}
-      </ul>
+        <p className="font-medium mt-4 break-words w-full ">
+          {project.description}
+        </p>
 
-      <a
-        className="cursor-pointer hover:text-primary-400 dark:hover:text-primary-700 shrink-0
+        <ul className="flex flex-wrap items-center gap-2 font-bold text-sm mt-4 pr-10 ">
+          {project.technologies.map((technology, techIndex) => (
+            <li
+              className="bg-tertiary-300 text-tertiary-800 py-1 px-3 rounded-xl"
+              key={techIndex}
+            >
+              {technology}
+            </li>
+          ))}
+        </ul>
+
+        <a
+          className="cursor-pointer hover:text-primary-400 dark:hover:text-primary-700 shrink-0
           absolute right-4 bottom-4"
-        href={project.githubUrl}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <AiFillGithub size={32} />
-      </a>
-    </article>
+          href={project.githubUrl}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {/* TODO - Tooltip Private respository */}
+          <AiFillLock
+            size={32}
+            data-tooltip-id={"private" + index}
+            className={`${
+              project.githubUrl ? "hidden" : "block"
+            } active:animate-shake outline-none`}
+          />
+          <AiFillGithub
+            size={32}
+            className={`${project.githubUrl ? "block" : "hidden"}`}
+          />
+        </a>
+      </article>
+    </div>
   ));
 
-  return <div className="flex">{projectsCard}</div>;
+  return (
+    <>
+      <h2>Projects</h2>
+
+      <p className="text-lg max-w-prose text">
+        Here are some of the projects I've worked on recently. Keep In mind that
+        some projects are not public for compoany security reasons. I'm always
+        working on something new, so check back soon for updates!
+      </p>
+      <div className="flex flex-wrap gap-6 justify-center mt-4">
+        {projectsCard}
+      </div>
+    </>
+  );
 }
