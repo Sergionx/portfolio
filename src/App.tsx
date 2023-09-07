a736d7003e4e492e124337a041b2beb28c2c4c22
Sergionx <sergio180203@gmail.com>
2023-09-07 10:48:15 -0400

    less padding top

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       dark:text-normal-white text-normal-black bg-normal-white dark:bg-normal-black"
       // transition-colors duration-500
     >
-      <section id="Home" className="p-5 max-w-3xl flex items-center pt-64">
+      <section id="Home" className="p-5 max-w-3xl flex items-center pt-32 md:pt-64">
         <Hero />
       </section>
 
