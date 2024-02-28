b8af598d4c3e58851c4505cf23d364b780f4c682
Sergionx <sergio180203@gmail.com>
2024-02-27 22:40:13 -0400

    Fix typos and improve readability in About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <h2>About Me</h2>
 
       <p className="text-lg font-medium max-w-prose ">
-        Since 2020 I began my journey as a software engineer, at the present I
+        Since 2020 I began my journey as a software engineer, in the present I
         am a{" "}
         <span className="font-bold text-gradient-color">
           fullstack developer
@@ -13,8 +13,8 @@ export default function About() {
       </p>
 
       <p className="text-lg font-medium  max-w-prose  mt-4 ">
-        The past year I deliver at my past company web applications using
-        principally React and Angular, and backend services using Firebase.
+        The past year I delivered at my past company web applications using
+        React and Angular, and backend services using Firebase.
         <span className="font-bold text-gradient-color">
           {" "}
           Improving the workflow and the delivery time
@@ -23,7 +23,7 @@ export default function About() {
       </p>
 
       <p className="text-lg font-medium  max-w-prose  mt-4 ">
-        As a programmer the 2 word that best defines me are{" "}
+        As a programmer, the 2 words that best defines me are{" "}
         <span className="font-bold text-gradient-color">adaptation</span> and
         <span className="font-bold text-gradient-color"> curiosity</span>. My
         goal is to learn something new every day, and I am always looking for
