e8aeeb541aa0ad33c833f9fd4bb3285b52d41b0f
Sergionx <sergio180203@gmail.com>
2024-07-13 17:25:58 -0400

    refactor: Move experiences data to separate file

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -1,3 +1,5 @@
+import { experiences } from "../../data/experiences";
+
 interface Experience {
   title: string;
   company: string;
@@ -6,42 +8,6 @@ interface Experience {
 }
 
 export default function Experience() {
-  const experiences = [
-    {
-      title: "Yummy Codicon",
-      company: "Yummy",
-      time: "August 2023",
-      description: `Selected to compete at a 50 people hackathon to
-        deliver a solution to an existent problem in a app. The teams were
-        formed by 5 people and we had 24 hours to deliver a solution. Our team
-        idea was to improve the user experience of Yummy Rides (a rideshare app)
-        by focusing in the inter-urban travels where the prices can go up to 200$.
-        We developed a system that allows to book a seat in a bus, plane or a individual 
-        car and provide which one is the best economic option 
-      `,
-    },
-    {
-      title: "FullStack Developer at Aether Solutions",
-      company: "Aether Solutions",
-      time: "September 2023",
-      description: `My responsabilities were to help any new or ongoing
-        project in multiple areas. The work methodology was
-        team based and I participated in 3 different groups
-        and projects. We used technologies such as Angular,
-        Tailwind, or Firebase and tools like Trello and Git
-        based repository for organization.`,
-    },
-    {
-      title: "Self-learning",
-      company: "",
-      time: "September 2022",
-      description: `My focus was to learn the bases of web development and
-        design. I learned HTML, CSS, JavaScript, React, and
-        Tailwind. I also learned how to integrate the frontend
-        with the backend using Express and MongoDB.`,
-    },
-  ];
-
   const experiencesList = experiences.map((experience, index) => (
     <li className="ml-4" key={index}>
       <div className="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
---
+++ src/data/experiences.ts
@@ -0,0 +1,42 @@
+export const experiences = [
+  {
+    title: "Frontend Engineer at JM Solutions",
+    company: "JM Solutions",
+    time: "April 2024 - Now",
+    description: `My main responsibility is to maintain the integrity for the frontend while also providing a good performance of the app. The project consist on an ERP and I have delivered tasks such as reducing a 30% of API calls and creating custom filters to query data.`,
+  },
+
+  {
+    title: "Yummy Codicon",
+    company: "Yummy",
+    time: "August 2023",
+    description: `Selected to compete at a 50 people hackathon to
+      deliver a solution to an existent problem in a app. The teams were
+      formed by 5 people and we had 24 hours to deliver a solution. Our team
+      idea was to improve the user experience of Yummy Rides (a rideshare app)
+      by focusing in the inter-urban travels where the prices can go up to 200$.
+      We developed a system that allows to book a seat in a bus, plane or a individual 
+      car and provide which one is the best economic option 
+    `,
+  },
+  {
+    title: "FullStack Developer at Aether Solutions",
+    company: "Aether Solutions",
+    time: "November 2022 - September 2023",
+    description: `My responsabilities were to help any new or ongoing
+      project in multiple areas. The work methodology was
+      team based and I participated in 3 different groups
+      and projects. We used technologies such as Angular,
+      Tailwind, or Firebase and tools like Trello and Git
+      based repository for organization.`,
+  },
+  {
+    title: "Self-learning",
+    company: "",
+    time: "September 2022",
+    description: `My focus was to learn the bases of web development and
+      design. I learned HTML, CSS, JavaScript, React, and
+      Tailwind. I also learned how to integrate the frontend
+      with the backend using Express and MongoDB.`,
+  },
+];
