0103474445fa74c0ad8907f934214bd261631e14
Sergionx <sergio180203@gmail.com>
2024-07-13 17:35:02 -0400

    chore: Update npm dependencies and add Next UI components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
@@ -67,7 +73,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 
 // https://uicolors.app/edit?sv1=my-sin:50-fffaeb/100-fef1c7/200-fee389/300-fecd4b/400-fdbb2d/500-f79609/600-db6f04/700-b64c07/800-933b0d/900-79310e/950-461702;jacksons-purple:50-eff5ff/100-dbe8fe/200-c0d7fd/300-94bffc/400-629cf8/500-3d77f4/600-2759e9/700-1f44d6/800-1f39ae/900-20368d/950-182253;roof-terracotta:50-fef2f2/100-fde3e3/200-fdcbcb/300-faa7a7/400-f57474/500-eb4848/600-d82a2a/700-b21f1f/800-961e1e/900-7d1f1f/950-440b0b
