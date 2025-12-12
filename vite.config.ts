4de6b229a3a300dc40bf4390f72cd915e8f5c2e0
Sergionx <sergio180203@gmail.com>
2025-12-12 18:09:49 -0400

    fix: downgrade @tanstack/start to version 1.114.5 and add nitro plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { devtools } from "@tanstack/devtools-vite";
 
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from 'nitro/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
+    nitro(),
     viteReact(),
     devtools(),
     tailwindcss(),
