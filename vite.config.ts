d5a39490aac7581875303510c296b0936530c87d
Sergionx <sergio180203@gmail.com>
2025-12-12 18:36:29 -0400

    refactor: remove nitro dependency and integrate netlify plugin in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { devtools } from "@tanstack/devtools-vite";
 
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from 'nitro/vite'
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
-    nitro(),
     viteReact(),
     devtools(),
+    netlify(),
     tailwindcss(),
   ],
 });
