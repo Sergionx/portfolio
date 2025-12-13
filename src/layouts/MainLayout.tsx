12caa7487949626b5628a705350a2e8c6e1d8215
Sergionx <sergio180203@gmail.com>
2025-12-12 21:25:50 -0400

    chore: update SideMenu import path for consistency

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "@tanstack/react-router";
-import SideMenu from "../components/navigation/SideMenu";
+import SideMenu from "@/components/navigation/SideMenu";
 
 export default function MainLayout() {
   return (
     <main
-      className="flex flex-col items-center  min-h-[200vh] px-8 md:px-48 transition-colors duration-500
+      className="flex flex-col items-center min-h-[200vh] px-8 md:px-48 transition-colors duration-500
       text-foreground bg-background"
     >
       <SideMenu />
